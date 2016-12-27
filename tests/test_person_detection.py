e887ba9300c3c3b4da06dc0b93290b859eec8f67
chris <cwmeijer@gmail.com>
2016-12-27 15:22:37 +0100

    refactor tests

--- tests/test_person_detection.py
+++ tests/test_person_detection.py
@@ -4,16 +4,21 @@ from settings import test_data_dir
 from os import path
 from keras.preprocessing.image import list_pictures
 
+
 class PersonDetectionSuite(unittest.TestCase):
     """ Tests. """
+
     def test_detect_from_files(self):
-        """ It should at least run without exceptions with two valid file paths. """
-        motion_detection_test_dir = path.join(test_data_dir, 'motion_detection')
-        images = list_pictures(motion_detection_test_dir)
-        for i, image in enumerate(images):
+        """ Should run without exceptions with two valid file paths. """
+        for i, image in enumerate(self.images):
             if i == 0:
                 continue
 
-            previous_image = images[i-1]
+            previous_image = self.images[i - 1]
             detect_person_from_files(previous_image, image)
 
+    def setUp(self):
+        self.motion_detection_test_dir = path.join(test_data_dir, 'motion_detection')
+        self.images = list_pictures(self.motion_detection_test_dir)
+        with open(path.join(self.motion_detection_test_dir, 'annotations')) as f:
+            self.annotations = [line.split(' ')[1] for line in f]
