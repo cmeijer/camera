87f59a46e8745484032ce040f3073079acfe0e0a
chris <cwmeijer@gmail.com>
2016-12-27 15:54:59 +0100

    add speed test and recall test

--- person_detection/__init__.py
+++ person_detection/__init__.py
@@ -4,6 +4,7 @@ from keras.preprocessing import image
 def detect_person_from_files(image_file1, image_file2):
     x1 = load_image(image_file1)
     x2 = load_image(image_file2)
+    return True
 
 
 def load_image(image_file):
--- tests/test_person_detection.py
+++ tests/test_person_detection.py
@@ -3,22 +3,64 @@ from person_detection import detect_person_from_files
 from settings import test_data_dir
 from os import path
 from keras.preprocessing.image import list_pictures
+from time import time
 
 
 class PersonDetectionSuite(unittest.TestCase):
     """ Tests. """
 
-    def test_detect_from_files(self):
+    def test_detect_from_files_no_exc(self):
         """ Should run without exceptions with two valid file paths. """
+        self.detect_in_images()
+
+    def test_detect_from_files_quick(self):
+        """ Should run under 1/10th of a second per image. """
+        print('\nSpeed test: ', end='')
+        n_images = len(self.images) - 1
+        start_time = time()
+
+        self.detect_in_images()
+
+        taken_per_image = (time() - start_time) / n_images
+        print('{} seconds per image for {} images'.format(taken_per_image, n_images))
+        assert taken_per_image < 0.1
+
+    def test_detect_from_files_recall(self):
+        """ Should have at least 90% recall. """
+        print('\nRecall test: ', end='')
+        results = self.detect_in_images()
+
+        retrieved = 0
+        total_positive = 0
+
+        for i in range(len(results)):
+            annotation = self.annotations[i]
+            result = results[i]
+            if annotation == self.positive_annotation:
+                total_positive += 1
+
+                if result:
+                    retrieved += 1
+
+        recall = retrieved / total_positive
+        print('Retrieved {} of {} positives ({} recall)'.format(retrieved, total_positive, recall))
+        assert recall > 0.9
+
+    def detect_in_images(self):
+        """ Run detection method over all images and return the results. """
+        results = []
         for i, image in enumerate(self.images):
             if i == 0:
                 continue
 
             previous_image = self.images[i - 1]
-            detect_person_from_files(previous_image, image)
+            results.append(detect_person_from_files(previous_image, image))
+        return results
 
     def setUp(self):
         self.motion_detection_test_dir = path.join(test_data_dir, 'motion_detection')
         self.images = list_pictures(self.motion_detection_test_dir)
         with open(path.join(self.motion_detection_test_dir, 'annotations')) as f:
-            self.annotations = [line.split(' ')[1] for line in f]
+            self.annotations = [line.split(' ')[1].strip('\n') for line in f]
+        self.positive_annotation = 'person_visible'
+        self.negative_annotation = 'no_person_visible'
