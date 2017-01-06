0d2b2e78435990787a6c99363c4a530c1353322f
chris <cwmeijer@gmail.com>
2017-01-06 07:33:43 +0100

    add wlan dao test that creates and deletes databases

--- wlan_dao.py
+++ wlan_dao.py
@@ -1,12 +1,20 @@
 import sqlite3
+import settings
 
+table_name = 'access'
+columns = ['ip', 'mac', 'time']
 
 
 def save_or_update(ip, mac, time):
     pass
 
-def get():
-    pass
+
+def get_time_by_mac(mac):
+    return None
+
+
+def setup():
+    create_table(settings.database, table_name, columns)
 
 
 def create_table(db_name, table_name, table_header):
