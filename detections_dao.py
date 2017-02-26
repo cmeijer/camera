d856eac0ae07723503a505fad56d313f53431495
chris <cwmeijer@gmail.com>
2017-02-26 20:11:19 +0100

    fix query for saving

--- detections_dao.py
+++ detections_dao.py
@@ -9,7 +9,7 @@ columns = ['id', 'image', 'object_type', 'boundingbox']
 
 def save_or_update(filename, object_type, bounding_box=None):
     new_id = uuid.uuid4()
-    query = 'INSERT INTO {} VALUES ("{}", "{}")'.format(table_name, new_id, filename, object_type, bounding_box)
+    query = 'INSERT INTO {} VALUES ("{}", "{}", "{}", "{}")'.format(table_name, new_id, filename, object_type, bounding_box)
     execute_query(settings.database, query)
 
 
