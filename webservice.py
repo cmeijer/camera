7fc1e027aea50aa6c6f1dca57c112c0e01968dc5
chris <cwmeijer@gmail.com>
2017-02-26 19:32:21 +0100

    add webservice

---
+++ webservice.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import jsonify
+
+import detections_dao
+import wlan_dao
+import images_dao
+
+"""
+This is a webservice for storing and retrieving information about observations.
+/images
+
+/detections
+
+
+tables:
+- images                [filename.jpg, datetime]
+
+- detections            [id, filename.jpg, objecttypeid, boundingbox]
+
+- devices               [mac, description]
+
+- connections           ['ip', mac_column, time_column, 'description']
+"""
+
+app = Flask(__name__)
+
+
+@app.route('/images')
+def get_images():
+    return jsonify(images_dao.get_all_images())
+
+
+@app.route('/detections')
+def get_detections():
+    return jsonify(detections_dao.get_all_detections())
+
+
+@app.route('/connections')
+def get_connections():
+    return jsonify(wlan_dao.get_all_connections())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
